054c88ccb38c0ebf7ea44da482694ba4d6a7a55d
WinniePokes <winnieyu.pro@gmail.com>
2025-12-11 11:09:23 -0500

    Added comments with correct structure

--- models/Bets.js
+++ models/Bets.js
@@ -1,11 +1,8 @@
 /**
- * WalletService
- * Manages user balances, deposits, withdrawals, and betting validations.
- * Used by SquadService and HabitService. 
- * 
+ * Bets Model
+ * This file defines the schema for the Bet model in the application.
  * @author Winnie Yu
  */
-
 
 const mongoose = require("mongoose");
 const { required } = require("nodemon/lib/config");
@@ -14,17 +11,17 @@ const { required } = require("nodemon/lib/config");
  * Bet Schema
  */
 const betSchema = new mongoose.Schema({
-  //
+  // Reference to the User who placed the bet
   User: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
-  //
+  // Description of the bet
   betAmount: { // e.g., "5"
     type: String,
     require: true,
   },
-  //
+  // Date when the bet was created
   createdAt: {
     type: Date,
     default: Date.now,
