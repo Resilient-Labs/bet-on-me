247d29f00e49cc8e1d77ee1611f92b5bc4e8153c
WinniePokes <winnieyu.pro@gmail.com>
2025-12-11 11:05:19 -0500

    Added comments with correct structure

--- models/Bets.js
+++ models/Bets.js
@@ -1,15 +1,30 @@
+/**
+ * WalletService
+ * Manages user balances, deposits, withdrawals, and betting validations.
+ * Used by SquadService and HabitService. 
+ * 
+ * @author Winnie Yu
+ */
+
+
 const mongoose = require("mongoose");
 const { required } = require("nodemon/lib/config");
 
+/**
+ * Bet Schema
+ */
 const betSchema = new mongoose.Schema({
+  //
   User: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+  //
   betAmount: { // e.g., "5"
     type: String,
     require: true,
   },
+  //
   createdAt: {
     type: Date,
     default: Date.now,
