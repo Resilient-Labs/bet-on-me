7f79f560a3bc9a11fcf63c25f2aa784ade0dd2c9
glitchwhostolexmas <justin.raf.jimenez@gmail.com>
2025-12-12 13:53:42 -0500

    cookie functionality and time storage

--- public/js/clock.js
+++ public/js/clock.js
@@ -16,9 +16,7 @@ Setting up a day to save this in localStorage so it doesn't go away as you navig
 
 
   (function() {
-    var countdown, init_countdown, set_countdown, userTargetDate, initialCountdownComplete;
-  
-    initialCountdownComplete = false;
+    var countdown, init_countdown, set_countdown, saveClockState, loadClockState;
   
     countdown = init_countdown = function() {
       countdown = new FlipClock($('.countdown'), {
@@ -28,32 +26,40 @@ Setting up a day to save this in localStorage so it doesn't go away as you navig
         countdown: true,
         callbacks: {
           start: function() {
-            return console.log('The clock has started!');
+            console.log('The clock has started!');
           },
           stop: function() {
             console.log('The clock has stopped!');
             
-            // Mark initial countdown as complete
-            initialCountdownComplete = true;
+            // Check if we need to switch to the final countdown
+            var initialComplete = $.cookie('initialCountdownComplete');
+            var userTargetDate = $.cookie('userTargetDate');
             
-            // Reset with countdown to user's target date when it reaches 0
-            if (userTargetDate) {
+            if (!initialComplete && userTargetDate) {
+              // Mark initial countdown as complete
+              $.cookie('initialCountdownComplete', 'true', { expires: 365 });
+              
               setTimeout(function() {
-                let currentDate = new Date();
-                let diff = userTargetDate / 1000 - currentDate.getTime() / 1000;
+                var currentDate = new Date();
+                var targetDate = new Date(parseInt(userTargetDate));
+                var diff = (targetDate.getTime() - currentDate.getTime()) / 1000;
                 
                 if (diff > 0) {
                   set_countdown(diff / 60, new Date());
-                  console.log('Resetting to target date countdown');
+                  console.log('Switching to target date countdown');
                 } else {
-                  console.log('Target date has passed, cannot restart');
+                  console.log('Target date has passed');
+                  countdown.setTime(0);
                 }
               }, 1000);
             }
           },
           interval: function() {
-            var time;
-            time = this.factory.getTime().time;
+            var time = this.factory.getTime().time;
+            
+            // Save current state every interval
+            saveClockState();
+            
             if (time <= 0) {
               console.log('Countdown reached 0!');
             }
@@ -61,6 +67,58 @@ Setting up a day to save this in localStorage so it doesn't go away as you navig
         }
       });
       return countdown;
+    };
+  
+    // Save the current clock state to cookies
+    saveClockState = function() {
+      if (countdown && countdown.running) {
+        var currentTime = countdown.getTime().time;
+        var now = new Date().getTime();
+        
+        $.cookie('clockTime', currentTime.toString(), { expires: 365 });
+        $.cookie('clockLastUpdate', now.toString(), { expires: 365 });
+      }
+    };
+  
+    // Load and resume clock state from cookies
+    loadClockState = function() {
+      var savedTime = $.cookie('clockTime');
+      var lastUpdate = $.cookie('clockLastUpdate');
+      var userTargetDate = $.cookie('userTargetDate');
+      var initialComplete = $.cookie('initialCountdownComplete');
+      
+      if (savedTime && lastUpdate) {
+        var now = new Date().getTime();
+        var elapsed = Math.floor((now - parseInt(lastUpdate)) / 1000);
+        var remainingTime = parseInt(savedTime) - elapsed;
+        
+        if (remainingTime > 0) {
+          console.log('Resuming clock with ' + remainingTime + ' seconds remaining');
+          document.getElementById('wholeClock').style.display = 'unset';
+          document.getElementById('challengeCall').disabled = true;
+          document.getElementById('joinChallenge').style.display = 'unset';
+          
+          countdown.setTime(remainingTime);
+          countdown.start();
+          return true;
+        } else if (initialComplete && userTargetDate) {
+          // Initial countdown finished, check if we should show final countdown
+          var currentDate = new Date();
+          var targetDate = new Date(parseInt(userTargetDate));
+          var diff = (targetDate.getTime() - currentDate.getTime()) / 1000;
+          
+          if (diff > 0) {
+            console.log('Resuming final countdown');
+            document.getElementById('wholeClock').style.display = 'unset';
+            document.getElementById('challengeCall').disabled = true;
+            document.getElementById('joinChallenge').style.display = 'unset';
+            
+            set_countdown(diff / 60, new Date());
+            return true;
+          }
+        }
+      }
+      return false;
     };
   
     set_countdown = function(minutes, start) {
@@ -80,36 +138,46 @@ Setting up a day to save this in localStorage so it doesn't go away as you navig
       
       countdown.setTime(left_secs);
       countdown.start();
+      saveClockState();
     };
   
+    // Initialize countdown
     init_countdown();
+    
+    // Try to load existing clock state on page load
+    loadClockState();
   
+    // Form submission handler
     $('form').on('submit', function(e) {
-      document.getElementById('wholeClock').style.display = 'unset'
-      document.getElementById('challengeCall').disabled = true
-      document.getElementById('joinChallenge').style.display = 'unset'
       e.preventDefault();
       
+      document.getElementById('wholeClock').style.display = 'unset';
+      document.getElementById('challengeCall').disabled = true;
+      document.getElementById('joinChallenge').style.display = 'unset';
+      
       // Get user input date and time
-      let userDateInput = $('#date-input').val(); // Format: "2026-01-01"
-      let userTimeInput = $('#time-input').val() || "10:00"; // Format: "10:00"
+      var userDateInput = $('#date-input').val();
+      var userTimeInput = $('#time-input').val() || "10:00";
       
-      // Set timezone
-      moment.tz.add('America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0');
+      // Create target date (assuming EST/EDT timezone)
+      var targetDateStr = userDateInput + " " + userTimeInput;
+      var targetDate = new Date(targetDateStr);
       
-      // Store the user's target date for later reset
-      userTargetDate = moment.tz(userDateInput + " " + userTimeInput, "America/New_York");
+      // Store the target date timestamp
+      $.cookie('userTargetDate', targetDate.getTime().toString(), { expires: 365 });
+      console.log("Target date set to: " + targetDate.toString());
       
-      console.log("Target date set to: " + userTargetDate.format());
+      // Check if initial countdown has been completed
+      var initialComplete = $.cookie('initialCountdownComplete');
       
-      // Always start with 2880.016 minutes on first run
-      if (!initialCountdownComplete) {
+      if (!initialComplete) {
+        // Start initial 48-hour countdown (2880.016 minutes)
         set_countdown(2880.016, new Date());
-        console.log("Starting initial countdown: 2880.016 minutes");
+        console.log("Starting initial countdown: 2880.016 minutes (48 hours)");
       } else {
-        // If already completed once, go straight to target date countdown
-        let currentDate = new Date();
-        let diff = userTargetDate / 1000 - currentDate.getTime() / 1000;
+        // Initial countdown already done, go straight to target date
+        var currentDate = new Date();
+        var diff = (targetDate.getTime() - currentDate.getTime()) / 1000;
         
         if (diff > 0) {
           set_countdown(diff / 60, new Date());
@@ -121,4 +189,128 @@ Setting up a day to save this in localStorage so it doesn't go away as you navig
       }
     });
   
+    // Optional: Clear all cookies button (for testing)
+    $('#resetChallenge').on('click', function() {
+      $.removeCookie('clockTime');
+      $.removeCookie('clockLastUpdate');
+      $.removeCookie('userTargetDate');
+      $.removeCookie('initialCountdownComplete');
+      location.reload();
+    });
+  
   }).call(this);
+
+
+
+  // (function() {
+  //   var countdown, init_countdown, set_countdown, userTargetDate, initialCountdownComplete;
+  
+  //   initialCountdownComplete = false;
+  
+  //   countdown = init_countdown = function() {
+  //     countdown = new FlipClock($('.countdown'), {
+  //       clockFace: 'DailyCounter',
+  //       language: 'en',
+  //       autoStart: false,
+  //       countdown: true,
+  //       callbacks: {
+  //         start: function() {
+  //           return console.log('The clock has started!');
+
+  //         },
+  //         stop: function() {
+  //           console.log('The clock has stopped!');
+            
+  //           // Mark initial countdown as complete
+  //           initialCountdownComplete = true;
+            
+  //           // Reset with countdown to user's target date when it reaches 0
+  //           if (!$.cookie('userTargetDate')) {
+  //             setTimeout(function() {
+  //               let currentDate = new Date();
+  //               let diff = userTargetDate / 1000 - currentDate.getTime() / 1000; 
+  //               // Might have to store line above in cookies //
+
+                
+  //               if (diff > 0) {
+  //                 set_countdown(diff / 60, new Date());
+  //                 console.log('Resetting to target date countdown');
+  //               } else {
+  //                 console.log('Target date has passed, cannot restart');
+  //               }
+  //             }, 1000);
+  //           }
+  //         },
+  //         interval: function() {
+  //           var time;
+  //           time = this.factory.getTime().time;
+  //           if (time <= 0) {
+  //             console.log('Countdown reached 0!');
+  //           }
+  //         }
+  //       }
+  //     });
+  //     return countdown;
+  //   };
+  
+  //   set_countdown = function(minutes, start) {
+  //     var elapsed, end, left_secs, now, seconds;
+      
+  //     seconds = minutes * 60;
+  //     now = new Date();
+  //     start = new Date(start);
+  //     end = start.getTime() + seconds * 1000;
+  //     left_secs = Math.round((end - now.getTime()) / 1000);
+  //     elapsed = false;
+      
+  //     if (left_secs < 0) {
+  //       left_secs *= -1;
+  //       elapsed = true;
+  //     }
+      
+  //     countdown.setTime(left_secs);
+  //     countdown.start();
+  //   };
+  
+  //   init_countdown();
+  
+  //   $('form').on('submit', function(e) {
+  //     document.getElementById('wholeClock').style.display = 'unset'
+  //     document.getElementById('challengeCall').disabled = true
+  //     document.getElementById('joinChallenge').style.display = 'unset'
+  //     e.preventDefault();
+      
+  //     // Get user input date and time
+  //     let userDateInput = $('#date-input').val(); // Format: "2026-01-01"
+  //     let userTimeInput = $('#time-input').val() || "10:00"; // Format: "10:00"
+      
+  //     // Set timezone
+  //     moment.tz.add('America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0');
+      
+  //     // Store the user's target date for later reset
+  //     userTargetDate = $.cookie(moment.tz(userDateInput + " " + userTimeInput, "America/New_York"));
+      
+  //     console.log("Target date set to: " + userTargetDate.format());
+      
+  //     // Always start with 2880.016 minutes on first run
+  //     if (!initialCountdownComplete) {
+  //      let joinChallengeCountdown = $.cookie(set_countdown(2880.016, new Date()));
+  //       console.log("Starting initial countdown: 2880.016 minutes");
+  //     } else {
+  //       // If already completed once, go straight to target date countdown
+  //       let currentDate = new Date();
+  //       $.removeCookie('joinChallengeCountdown')
+  //       let diff = $.cookie('userTargetDate ')/ 1000 - currentDate.getTime() / 1000;
+        
+  //       if (diff > 0) {
+  //         set_countdown(diff / 60, new Date());
+  //         console.log("Starting target date countdown");
+  //       } else {
+  //         console.log("Target date has already passed!");
+  //         $.removeCookie('userTargetDate');
+  //         countdown.setTime(0);
+  //       }
+  //     }
+  //   });
+  
+  // }).call(this);
