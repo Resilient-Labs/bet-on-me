7690f14390aa352800d208efe5f80e6d2a8d6ca1
Justin Joshi <justinjoshi@proton.me>
2025-12-11 20:05:13 -0500

    adding changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -137,3 +137,68 @@ exports.postSignup = async (req, res, next) => {
       });
   });
 };
+
+
+
+/* this functionality will make use of the following dependencies -- async, nodemailer, crypto --  to create a token, and send an email to a user to change their forgotten password
+
+ *async - provides .waterfall() method -
+ **from the async docs:
+ ***Runs the tasks array of functions in series, each passing their results to the next in the array. However, if any of the   tasks pass an error to their own callback, the next function is not executed, and the main callback is immediately called with the error.
+
+*nodemailer - sends an email to the user's registered email address using SMTP
+
+*crypto - creates a random token to send to user's email to verify their password!
+
+* Author: @JustinJoshi
+*/
+exports.forgotPassword = function(req, res, next) {
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/forgot');
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport('SMTP', {
+        service: 'gmail',
+        auth: {
+          user: '!!! YOUR SENDGRID USERNAME !!!',
+          pass: '!!! YOUR SENDGRID PASSWORD !!!'
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'passwordreset@demo.com',
+        subject: 'Node.js Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/forgot');
+  });
+};
