ad0a2c085985b9291a60ed9f3a9e48aaadcca67e
ANGÉL <asa@ANGEL.local>
2025-12-13 17:41:05 -0500

    updating Goal input

--- controllers/goals.js
+++ controllers/goals.js
@@ -10,6 +10,44 @@ module.exports = {
     }
   },
 
+  // Create / Update One Goal
+  // get the name from the req.body
+  // try to find one goal & update
+  // if there is no goal, we will create it
+  // redirect to createGoal
+  createOrUpdateGoal : async (req, res) => {
+    try {
+      const { name, description, completed } = req.body;
+
+      const updatedGoal = await Goal.findOneAndUpdate({
+        user: req.user.id,
+      },{
+        name
+      })
+      console.log(updatedGoal)
+
+      //if we don't hv the goal yet, disregard findone andupdate one bcs it's null
+
+      //prevent submit if the input is empty
+      //have the original name inside input field
+      //if the name is empty on the backend, we just ignore
+
+      if (updatedGoal == null) {
+        const goal = await Goal.create({
+        name,
+        description,
+        completed: completed || false,
+        user: req.user.id,
+      });
+      }
+      res.redirect("/userGoal");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server error");
+    }
+  },
+
+
   // create a goal
   createGoal: async (req, res) => {
     try {
